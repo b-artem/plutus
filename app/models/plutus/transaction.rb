c19ba1dcf0b7c9d397b472e7eb828f0151ea4c7b
Dmitri Zutskov <keeruline@gmail.com>
2019-01-24 13:30:58 +0200

    backporting association definition from master, otherwise accepts_nested_attributes_for do not work and it is not possible to override has_many in class_eval

--- app/models/plutus/transaction.rb
+++ app/models/plutus/transaction.rb
@@ -25,10 +25,10 @@ module Plutus
     attr_accessible :description, :commercial_document
 
     belongs_to :commercial_document, :polymorphic => true
-    has_many :credit_amounts, :extend => AmountsExtension
-    has_many :debit_amounts, :extend => AmountsExtension
-    has_many :credit_accounts, :through => :credit_amounts, :source => :account
-    has_many :debit_accounts, :through => :debit_amounts, :source => :account
+    has_many :credit_amounts, :extend => AmountsExtension, :class_name => 'Plutus::CreditAmount', :inverse_of => :transaction
+    has_many :debit_amounts, :extend => AmountsExtension, :class_name => 'Plutus::DebitAmount', :inverse_of => :transaction
+    has_many :credit_accounts, :through => :credit_amounts, :source => :account, :class_name => 'Plutus::Account'
+    has_many :debit_accounts, :through => :debit_amounts, :source => :account, :class_name => 'Plutus::Account'
 
     validates_presence_of :description
     validate :has_credit_amounts?
